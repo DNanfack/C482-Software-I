d0d904e6a99c4eaab60f360a5f27f4f9f8948a07
RBertoCases <roberto.cases@gmail.com>
2017-11-05 08:03:13 -0800

    reconfigured folder hierarchy

--- RCasesInventorySystem/src/rcases/inventory.java
+++ RCasesInventorySystem/src/rcases/inventory.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package rcasesinventorysystem;
+package rcases;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -15,7 +15,7 @@ import javafx.stage.Stage;
  *
  * @author rober
  */
-public class RCasesInventorySystem extends Application {
+public class inventory extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
