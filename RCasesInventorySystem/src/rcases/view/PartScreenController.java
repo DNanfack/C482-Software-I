2658f753ace5b535d7822350114d0b3824a5ff56
RBertoCases <roberto.cases@gmail.com>
2017-11-18 21:41:44 -0800

    added toggle functionality to PartScreen

--- RCasesInventorySystem/src/rcases/view/PartScreenController.java
+++ RCasesInventorySystem/src/rcases/view/PartScreenController.java
@@ -1,10 +1,13 @@
 package rcases.view;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 public class PartScreenController {
 
@@ -27,7 +30,7 @@ public class PartScreenController {
     private TextField partPricefield;
 
     @FXML
-    private TextField companyNameField;
+    private TextField companyMachineField;
 
     @FXML
     private TextField partMaxField;
@@ -36,19 +39,32 @@ public class PartScreenController {
     private TextField PartMinfield;
 
     @FXML
-    private Label machineIDLabel;
-
-    @FXML
     private RadioButton outsourcedRadioButton;
-
+    
     @FXML
-    void inhouseRadioHandler(ActionEvent event) {
+    private ToggleGroup partToggleGroup;
+        
+    
+    public void radioHandler()
+     {
+         if (this.partToggleGroup.getSelectedToggle().equals(this.inhouseRadioButton)){
+            companyMachineLabel.setText("Machine ID");
+            companyMachineField.setPromptText("Machine ID");
+         }
+         if (this.partToggleGroup.getSelectedToggle().equals(this.outsourcedRadioButton)){
+            companyMachineLabel.setText("Company Name");
+            companyMachineField.setPromptText("Company Name");
+         }
+     }
 
+    /**
+     *
+     * @param url
+     * @param rb
+     */
+    public void initialize(URL url, ResourceBundle rb) {
+    partToggleGroup = new ToggleGroup();
+    this.inhouseRadioButton.setToggleGroup(partToggleGroup);
+    this.outsourcedRadioButton.setToggleGroup(partToggleGroup);
     }
-
-    @FXML
-    void outsourcedRadioHandler(ActionEvent event) {
-
-    }
-
 }
