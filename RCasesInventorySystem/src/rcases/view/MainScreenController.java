5c93fcbcab4713db706f37b579573f7410dbd80c
RBertoCases <roberto.cases@gmail.com>
2017-11-25 02:42:18 -0800

    Able to create inhouse part and have it populate tableview on mainscreen

--- RCasesInventorySystem/src/rcases/view/MainScreenController.java
+++ RCasesInventorySystem/src/rcases/view/MainScreenController.java
@@ -1,16 +1,24 @@
 package rcases.view;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import rcases.model.Part;
+import static rcases.model.Inventory.getAllParts;
 
-public class MainScreenController {
+public class MainScreenController implements Initializable {
 
     @FXML // fx:id="searchPartsfield"
     private TextField searchPartsfield; // Value injected by FXMLLoader
@@ -22,8 +30,16 @@ public class MainScreenController {
     private Button addPartButton;
     
     @FXML
-    private Button button2;
-
+    private TableView<Part> partsTableView;
+    @FXML
+    private TableColumn<Part, Integer> partsIDColumn;
+    @FXML
+    private TableColumn<Part, String> partsNameColumn;
+    @FXML
+    private TableColumn<Part, Integer> partsInStockColumn;
+    @FXML
+    private TableColumn<Part, Double> partsPriceColumn;
+    
     @FXML
     void exitHandler(ActionEvent event) {
 
@@ -31,20 +47,18 @@ public class MainScreenController {
 
     @FXML
     void partsAddHandler(ActionEvent event) throws IOException{
-         Stage stage; 
-     Parent root;
+     Stage stage = null; 
+     Parent root = null;
      if(event.getSource()==addPartButton){
         //get reference to the button's stage         
         stage=(Stage) addPartButton.getScene().getWindow();
         //load up OTHER FXML document
-  root = FXMLLoader.load(getClass().getResource("/rcases/view/PartScreen.fxml"));
-      }
-     else{
-       stage=(Stage) button2.getScene().getWindow();
-  root = FXMLLoader.load(getClass().getResource("/rcases/view/MainScreen.fxml"));
-      }
+        root = FXMLLoader.load(getClass().getResource("/rcases/view/PartScreen.fxml"));
+        }
+     
      //create a new scene with root and set the stage
-      Scene scene = new Scene(root);
+      Scene scene;
+      scene = new Scene(root);
       stage.setScene(scene);
       stage.show();
     }
@@ -84,6 +98,24 @@ public class MainScreenController {
     @FXML
     void productsSearchHandler(ActionEvent event) {
 
+    }
+    
+    /**
+     *
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        partsIDColumn.setCellValueFactory(
+                new PropertyValueFactory<>("partID"));
+        partsNameColumn.setCellValueFactory(
+                new PropertyValueFactory<>("name"));
+        partsInStockColumn.setCellValueFactory(
+                new PropertyValueFactory<>("inStock"));
+        partsPriceColumn.setCellValueFactory(
+                new PropertyValueFactory<>("price"));
+        partsTableView.setItems(getAllParts());
     }
 
 }
--- RCasesInventorySystem/src/rcases/view/PartScreenController.java
+++ RCasesInventorySystem/src/rcases/view/PartScreenController.java
@@ -1,15 +1,24 @@
 package rcases.view;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
+import rcases.model.InhousePart;
+import rcases.model.Inventory;
 
-public class PartScreenController {
+public class PartScreenController implements Initializable  {
 
     @FXML
     private RadioButton inhouseRadioButton;
@@ -21,13 +30,13 @@ public class PartScreenController {
     private TextField partIDField;
 
     @FXML
-    private TextField partNamefield;
+    private TextField partNameField;
 
     @FXML
-    private TextField partInvField;
+    private TextField partInStockField;
 
     @FXML
-    private TextField partPricefield;
+    private TextField partPriceField;
 
     @FXML
     private TextField companyMachineField;
@@ -36,17 +45,25 @@ public class PartScreenController {
     private TextField partMaxField;
 
     @FXML
-    private TextField PartMinfield;
+    private TextField PartMinField;
 
     @FXML
     private RadioButton outsourcedRadioButton;
     
     @FXML
     private ToggleGroup partToggleGroup;
-        
     
+    @FXML 
+    private Button partSaveButton;
+    
+    @FXML
+    private Button partCancelButton;
+    
+    private int partID;
+                    
+    @FXML
     public void radioHandler()
-     {
+    {
          if (this.partToggleGroup.getSelectedToggle().equals(this.inhouseRadioButton)){
             companyMachineLabel.setText("Machine ID");
             companyMachineField.setPromptText("Machine ID");
@@ -55,16 +72,55 @@ public class PartScreenController {
             companyMachineLabel.setText("Company Name");
             companyMachineField.setPromptText("Company Name");
          }
-     }
-
+    }
+    
+    
+    @FXML
+    void partButtonHandler(ActionEvent event) throws IOException{
+     Stage stage; 
+     Parent root;
+     if(event.getSource()==partSaveButton){
+        String name = partNameField.getText();
+        String inStock = partInStockField.getText();
+        String price = partPriceField.getText();
+        String min = PartMinField.getText();
+        String max = partMaxField.getText();
+        String machineID = companyMachineField.getText();
+        if ((this.partToggleGroup.getSelectedToggle().equals(this.inhouseRadioButton))) {
+            InhousePart inPart = new InhousePart();
+            inPart.setPartID(partID);
+            inPart.setName(name);
+            inPart.setPrice(Double.parseDouble(price));
+            inPart.setInStock(Integer.parseInt(inStock));
+            inPart.setMin(Integer.parseInt(min));
+            inPart.setMax(Integer.parseInt(max));
+            inPart.setMachineID(Integer.parseInt(machineID));
+            Inventory.addPart(inPart);
+            } 
+        stage=(Stage) partSaveButton.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("/rcases/view/MainScreen.fxml"));
+      }
+     else{
+        stage=(Stage) partCancelButton.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("/rcases/view/MainScreen.fxml"));
+      }
+     //create a new scene with root and set the stage
+      Scene scene = new Scene(root);
+      stage.setScene(scene);
+      stage.show();
+    }
     /**
      *
      * @param url
      * @param rb
      */
+    @Override
     public void initialize(URL url, ResourceBundle rb) {
     partToggleGroup = new ToggleGroup();
     this.inhouseRadioButton.setToggleGroup(partToggleGroup);
     this.outsourcedRadioButton.setToggleGroup(partToggleGroup);
+    partID = Inventory.getPartIDCount();
+    partIDField.setText("Auto-Gen: " + partID);
     }
+     
 }
