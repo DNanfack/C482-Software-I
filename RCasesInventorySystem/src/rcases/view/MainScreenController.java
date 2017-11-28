6348faa6d5ce029186f9aea42f2cc0f7a9bb7e33
RBertoCases <roberto.cases@gmail.com>
2017-11-27 17:25:22 -0800

    Enable the user to search the partTableView on the main screen.

--- RCasesInventorySystem/src/rcases/view/MainScreenController.java
+++ RCasesInventorySystem/src/rcases/view/MainScreenController.java
@@ -3,20 +3,22 @@ package rcases.view;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.collections.transformation.FilteredList;
-import javafx.collections.transformation.SortedList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import rcases.model.Inventory;
 import rcases.model.Part;
 import static rcases.model.Inventory.getAllParts;
 
@@ -45,6 +47,7 @@ public class MainScreenController implements Initializable {
   
     @FXML
     private TableColumn<Part, Double> partsPriceColumn;
+    
     
     @FXML
     void exitHandler(ActionEvent event) {
@@ -80,10 +83,65 @@ public class MainScreenController implements Initializable {
     void partsModifyHandler(ActionEvent event) {
 
     }
+    
+    @FXML
+    public ObservableList<Part> tempPart=FXCollections.observableArrayList();
 
     @FXML
     void partsSearchHandler(ActionEvent event) {
 
+    String searchItem=searchPartsField.getText();
+        if (searchItem.equals("")){
+                partsTableView.setItems(getAllParts());
+        }
+          else{
+        boolean found=false;
+        try{
+        int itemNumber=Integer.parseInt(searchItem);
+        for(Part p: Inventory.getAllParts()){
+            if(p.getPartID()==itemNumber){
+                System.out.println("This is part "+ itemNumber);
+                found=true;
+                tempPart.clear();
+                tempPart.add(p);
+                partsTableView.setItems(tempPart);
+            
+            }
+            
+        }
+            if (found==false){
+            partsTableView.setItems(getAllParts());
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Information Dialog");
+            alert.setHeaderText("Error!");
+            alert.setContentText("Part not found");
+
+            alert.showAndWait();
+        }
+    }
+    catch(NumberFormatException e){
+        for(Part p: Inventory.getAllParts()){
+            if(p.getName().equals(searchItem)){
+                System.out.println("This is part "+p.getPartID());
+                found=true;
+                tempPart.clear();
+                tempPart.add(p);
+                partsTableView.setItems(tempPart);
+            }
+            
+        }
+            if (found==false){
+            partsTableView.setItems(getAllParts());
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Information Dialog");
+            alert.setHeaderText("Error");
+            alert.setContentText("Part not found");
+            
+            alert.showAndWait();
+            }
+    }
+    }
+    
     }
 
     @FXML
@@ -122,28 +180,7 @@ public class MainScreenController implements Initializable {
         partsPriceColumn.setCellValueFactory(
                 new PropertyValueFactory<>("price"));
         partsTableView.setItems(getAllParts());
-        FilteredList<Part> filteredParts = new FilteredList<>(getAllParts(), p -> true);
         
-        searchPartsField.textProperty().addListener((observable, oldValue, newValue) -> {
-            filteredParts.setPredicate(part -> {
-                if (newValue == null || newValue.isEmpty()) {
-                    return true;
-                }
-                String lowerCaseFilter = newValue.toLowerCase();
-
-                if (part.getName().toLowerCase().contains(lowerCaseFilter)) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-            );
-        }
-        );
-
-        SortedList<Part> sortedParts = new SortedList<>(filteredParts);
-        sortedParts.comparatorProperty().bind(partsTableView.comparatorProperty());
-        partsTableView.setItems(sortedParts);
     }
 
 }
