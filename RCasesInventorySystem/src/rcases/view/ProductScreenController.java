fad64d21c5e86e18c39bfe8611e3eb3adf766b0d
Roberto Cases <31931337+RBertoCases@users.noreply.github.com>
2017-12-07 09:42:06 -0800

    Update ProductScreenController.java
    Fleshed out ProductScreenController

--- RCasesInventorySystem/src/rcases/view/ProductScreenController.java
+++ RCasesInventorySystem/src/rcases/view/ProductScreenController.java
@@ -26,20 +26,69 @@ public class ProductScreenController {
 
     @FXML
     private TextField productSearchField;
+    
+    @FXML
+    private TableView<Part> partsTableView;
+    
+    @FXML
+    private TableColumn<Part, Integer> partsIDColumn;
+    
+    @FXML
+    private TableColumn<Part, String> partsNameColumn;
+    
+    @FXML
+    private TableColumn<Part, Integer> partsInStockColumn;
+    
+    @FXML
+    private TableColumn<Part, Double> partsPriceColumn;
+    
+    @FXML
+    private TableView<Part> associatedPartsTableView;
+    
+    @FXML
+    private TableColumn<Part, Integer> associatedPartsIDColumn;
+    
+    @FXML
+    private TableColumn<Part, String> associatedPartsNameColumn;
+    
+    @FXML
+    private TableColumn<Part, Integer> associatedPartsInStockColumn;
+    
+    @FXML
+    private TableColumn<Part, Double> associatedPartsPriceColumn;
+    
+    private int productID;
+    private ObservableList<Part> currentParts = FXCollections.observableArrayList();
+    public ObservableList<Part> tempPart=FXCollections.observableArrayList();
+    
+    @FXML
+    private void initialize() {
+        productID = Inventory.getProductIDCount();
+        productIDField.setText("Auto-Generated: " + productID);
+    }
+    
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
 
     @FXML
     void partsToProductHandler(ActionEvent event) {
-
+        Part part = partsTableView.getSelectionModel().getSelectedItem();
+        currentParts.add(part);
+        associatedPartsTableView.setItems(currentParts);
     }
 
     @FXML
     void productCancelHandler(ActionEvent event) {
-
+        productID = Inventory.cancelProductIDCount();
+        dialogStage.close();
     }
 
     @FXML
     void productDeleteHandler(ActionEvent event) {
-
+        Part part = AssociatedPartsTableView.getSelectionModel().getSelectedItem();
+        currentParts.remove(part);
+        associatedPartsTableView.setItems(currentParts);
     }
 
     @FXML
@@ -49,7 +98,74 @@ public class ProductScreenController {
 
     @FXML
     void productSearchHandler(ActionEvent event) {
+        String searchItem=searchPartsField.getText();
+        if (searchItem.equals("")){
+                partsTableView.setItems(getAllParts());
+        } else{
+            boolean found=false;
+            try{
+                int itemNumber=Integer.parseInt(searchItem);
+                Part p = Inventory.lookupPart(itemNumber);
+                if(p != null){
+                    found=true;
+                    tempPart.clear();
+                    tempPart.add(p);
+                    partsTableView.setItems(tempPart);
+                } 
+                if (found==false){
+                partsTableView.setItems(getAllParts());
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Error");
+                alert.setHeaderText("Part Not Found");
+                alert.setContentText("Please search by exact name or ID");
 
+                alert.showAndWait();
+            }
+        }
+        catch(NumberFormatException e){
+            for(Part p: Inventory.getAllParts()){
+                if(p.getName().equals(searchItem)){
+                    System.out.println("This is part "+p.getPartID());
+                    found=true;
+                    tempParts.clear();
+                    tempParts.add(p);
+                    partsTableView.setItems(tempPart);
+                }
+            
+            }
+            if (found==false){
+            partsTableView.setItems(getAllParts());
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Error");
+            alert.setHeaderText("Part Not Found");
+            alert.setContentText("Please search by exact name or ID");
+            
+            alert.showAndWait();
+            }
+        }
+        }
     }
+    
+    public void setProduct(Product product) {
+        selectedProduct = product;
+        
+        partIDField.setText(Integer.toString(part.getPartID()));
+        partNameField.setText(part.getName());
+        partInStockField.setText(Integer.toString(part.getInStock()));
+        partPriceField.setText(Double.toString(part.getPrice()));
+        PartMinField.setText(Integer.toString(part.getMin()));
+        partMaxField.setText(Integer.toString(part.getMax()));
+
+        if (part instanceof InhousePart) {
+            selectedInPart = (InhousePart) part;
+            companyMachineLabel.setText("Machine ID");
+            inhouseRadioButton.selectedProperty().set(true);
+            companyMachineField.setText(Integer.toString(selectedInPart.getMachineID()));
+        } else {
+            selectedOutPart = (OutsourcedPart) part;
+            companyMachineLabel.setText("Company Name");
+            outsourcedRadioButton.selectedProperty().set(true);
+            companyMachineField.setText(selectedOutPart.getCompanyName());
+        }
 
 }
